30c607abb828b919767c4747a20976398cdc97d6
tygo-payple <ty.go.payple.kr>
2024-10-24 15:50:21 +0900

    무이자_할부 안내 작업 완료

--- pagelist/data/data.js
+++ pagelist/data/data.js
@@ -119,7 +119,7 @@ data.modal = {
             page: 'confirm'
         },
         {
-            stateClass: 'working',
+            stateClass: 'complete',
             url: '전자금융거래_이용약관',
             category: 'modal',
             page: '전자금융거래_이용약관'
@@ -131,7 +131,7 @@ data.modal = {
             page: '개인정보수집_및_이용동의'
         },
         {
-            stateClass: 'working',
+            stateClass: 'complete',
             url: '개인정보제공_및_위탁동의',
             category: 'modal',
             page: '개인정보제공_및_위탁동의'
@@ -141,6 +141,12 @@ data.modal = {
             url: '정기결제_이용약관',
             category: 'modal',
             page: '정기결제_이용약관'
+        },
+        {
+            stateClass: 'complete',
+            url: '무이자_할부_안내',
+            category: 'modal',
+            page: '무이자_할부_안내'
         },
 
 
